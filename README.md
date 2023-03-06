05569a6ae821ee0284c39c02fd1bc019a9d9cf20
Gilberto Espinosa <gilbertosilvaespinosa@gmail.com>
2023-03-06 18:57:05 -0300

    finalizing project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# TarefasApi
-Minimal API usando Dapper e SQL server
+# Projeto - Criando uma minimal API usando o Dapper e o SQL Server
+## ✨ _TarefasApi_✨
+
+Uma minimal Api usando os recursos Dapper, definindo o mapeamento, simplificando o código da classe Program.cs  e utilizando os métodos de extensão ApplicationBuilder e webApplication.
+
+
+
+-Esse projeto condensa um pouco do meu processo no curso Web API.NET Core Essencial.
+
+-o link do curso >>> https://www.udemy.com/course/curso-web-api-asp-net-core-essencial/
--- TarefasApi/TarefasApi/Endpoints/TarefasEndpoints.cs
+++ TarefasApi/TarefasApi/Endpoints/TarefasEndpoints.cs
@@ -14,11 +14,48 @@ namespace TarefasApi.Endpoints
             {
                 using var con = await connectionGetter();
                 var tarefas = con.GetAll<Tarefa>().ToList();
-                               
-                if(tarefas is null)
+
+                if (tarefas is null)
                     return Results.NotFound();
 
                 return Results.Ok(tarefas);
+            });
+
+            app.MapGet("/tarefas/{id}", async (GetConection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                //var tarefa = con.Get<Tarefa>(id);
+                //if (tarefa is null)
+                //    return Results.NotFound();
+                //return Results.Ok(tarefa);
+
+                return con.Get<Tarefa>(id) is Tarefa tarefa ? Results.Ok(tarefa) : Results.NotFound();
+            });
+
+            app.MapPost("/tarefas", async (GetConection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Insert(tarefa);
+                return Results.Created($"/tarefas/{id}",tarefa);
+            });
+
+            app.MapPut("/tarefas", async (GetConection connectionGetter, Tarefa tarefa) =>
+            {
+                using var con = await connectionGetter();
+                var id = con.Update(tarefa);
+                return Results.Ok();
+            });
+
+            app.MapDelete("/tarefas/{id}", async (GetConection connectionGetter, int id) =>
+            {
+                using var con = await connectionGetter();
+                
+                var deleted = con.Get<Tarefa>(id);
+
+                if (deleted is null) return Results.NotFound();
+               
+                con.Delete(deleted);
+                return Results.Ok(deleted);
             });
         }
     }
